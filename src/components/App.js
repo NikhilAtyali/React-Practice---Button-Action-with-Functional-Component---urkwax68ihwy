4ed95e3e237f0d5138bf8c5aecf4e036cf05d319
NikhilAtyali <88334264+NikhilAtyali@users.noreply.github.com>
2022-01-04 23:20:28 +0530

    functional componenet

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,21 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [display, setDisplay] = useState({
+    pHidden:true
+  })
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id='click' onClick={()=>{
+        setDisplay({pHidden:false})
+      }} >Click</button>
+      {
+        display.pHidden?null: <p id='para'>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+        </p>
+     
+}
     </div>
   );
 }
